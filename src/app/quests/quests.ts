11816ebbc14c1cc93d335c6263b5732f3a336320
martasskv5 <145648867+martasskv5@users.noreply.github.com>
2025-09-26 18:24:06 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/quests/quests.ts
+++ src/app/quests/quests.ts
@@ -1,4 +1,3 @@
-import { isPlatformBrowser } from '@angular/common';
 import { Component } from '@angular/core';
 import { Card } from './card/card';
 
@@ -7,6 +6,7 @@ export type Quest = {
   title: string;
   description: string;
   completed: boolean;
+  xp: number;
 };
 
 @Component({
@@ -23,18 +23,21 @@ export class Quests {
       title: 'Find the Lost Sword',
       description: 'Retrieve the legendary sword from the ancient ruins.',
       completed: false,
+      xp: 100,
     },
     {
       id: 2,
       title: 'Rescue the Villagers',
       description: 'Save the villagers captured by goblins.',
       completed: true,
+      xp: 200,
     },
     {
       id: 3,
       title: 'Collect Herbs',
       description: 'Gather 10 healing herbs for the village healer.',
       completed: false,
+      xp: 50,
     },
   ];
   questFormVisible = false;
@@ -42,7 +45,7 @@ export class Quests {
     // Logic to show the quest creation form
     this.questFormVisible = !this.questFormVisible;
   }
-  deleteQuest(index: number) {
-    this.quests.splice(index, 1);
+  deleteQuest(id: number) {
+    this.quests = this.quests.filter((quest) => quest.id !== id);
   }
 }
