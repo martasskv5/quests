741bdd80b0997d4fc610dd4095bba72492fd233e
martasskv5 <145648867+martasskv5@users.noreply.github.com>
2025-11-16 18:03:55 +0100

    Add player level filtering; update player data handling and enhance player selection UI

--- src/app/players/players.ts
+++ src/app/players/players.ts
@@ -1,21 +1,38 @@
-import { Component, computed, inject, Signal, WritableSignal, signal } from '@angular/core';
-import { Player } from '../modules';
+import { Component, computed, inject, WritableSignal, signal } from '@angular/core';
+import { Player, playerLevels } from '../modules';
 import { PlayersService } from '../players';
 import { Card } from './card/card';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-players',
-  imports: [Card, RouterLink],
-  templateUrl: './players.html',
-  styleUrl: './players.scss'
+    selector: 'app-players',
+    imports: [Card, RouterLink, FormsModule],
+    templateUrl: './players.html',
+    styleUrls: ['./players.scss'],
 })
 export class Players {
     playersService = inject(PlayersService);
     playersData: WritableSignal<Player[]> = signal(this.playersService.getPlayers());
+    playerLevels = playerLevels;
+    selectedLevel: WritableSignal<string> = signal('all');
 
     handleDeletePlayer(username: string) {
         this.playersService.deletePlayerByUsername(username);
         this.playersData.set(this.playersService.getPlayers());
+    }
+
+    filteredPlayers = computed(() => {
+        const level = this.selectedLevel();
+        console.log(level);
+
+        if (level === 'all') {
+            return this.playersData();
+        }
+        return this.playersData().filter(player => this.playersService.getPlayerLevel(player.username).title === level);
+    });
+
+    onLevelChange(value: string) {
+        this.selectedLevel.set(value);
     }
 }
